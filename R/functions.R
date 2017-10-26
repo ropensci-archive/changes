e28fb17764790f95a0f446623d166b10c29593aa
Tim Churches <tim.churches@gmail.com>
2017-10-26 16:37:09 +1100

    Change argument to set_reminder()

--- R/functions.R
+++ R/functions.R
@@ -297,13 +297,14 @@ checkout_branch <- function (branch) {
   
 }
 
-set_reminder <- function(seconds=0, minutes=0, hours=0, days=0) {
-  # record reminder delay in seconds somewhere 
+set_reminder <- function(minutes) {
+  # convert minutes to seconds of reminder delay in seconds somewhere 
 }
 
+# This needs to be called at the tope of every stow function,
+# as well as in the .onLoad of the package
 schedule_reminder <- function() {
   # get the reminder delay from wherever
   later::later(function() { print( changes())}, delay=delay)
 }
 
-}
