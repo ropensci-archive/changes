8ad19d88f5a1e038709d80231013da5fb1941555
Tim Churches <tim.churches@gmail.com>
2017-10-27 06:59:03 +1100

    Try to get the cache working

--- R/functions.R
+++ R/functions.R
@@ -298,13 +298,15 @@ checkout_branch <- function (branch) {
 }
 
 set_reminder <- function(minutes) {
-  # convert minutes to seconds of reminder delay in seconds somewhere 
+  # convert minutes to seconds of reminder delay in seconds in cache
+  cache[["reminder_delay"]] <- minutes * 60
 }
 
 # This needs to be called at the tope of every stow function,
 # as well as in the .onLoad of the package
 schedule_reminder <- function() {
-  # get the reminder delay from wherever
+  # get the reminder delay from cache
+  delay <- cache[["reminder_delay"]]
   later::later(function() { print( changes())}, delay=delay)
 }
 
