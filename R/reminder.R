7c106b1e80bc888bb3b9a7d8a6dfcddd2f6eb403
Tim Churches <tim.churches@gmail.com>
2017-10-27 10:12:29 +1100

    Reminders working, sort of.

--- R/reminder.R
+++ R/reminder.R
@@ -1,14 +1,36 @@
-
-set_reminder <- function(minutes) {
+#' Set the reminder delay in minutes for commit reminder messages
+#'
+#' TODO: Describe this better.
+#'
+#' @param minutes Delay in minutes
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+reminder_delay <- function(minutes) {
+  assertCount(minutes)
   # convert minutes to seconds of reminder delay, store in .cache environment
   .cache[["reminder_delay"]] <- minutes*60
+  invisible(NULL)
 }
 
-# This needs to be called at the tope of every stow function,
-# as well as in the .onLoad of the package
+#' Schedule a reminder
+#'
+#' TODO: This needs to be called at the tope of every stow function, as well as in the .onLoad of the package
+#'
+#' @param 
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
 schedule_reminder <- function() {
-  # get the reminder delay from wherever
+  # get the reminder delay from the .cache environment
   delay <- .cache$reminder_delay
-  later::later(function() { print(getwd())}, delay=delay)
+  if (delay > 0) {
+    later::later(~cat("Hey, you should commit your changes!\n"), delay=delay)
+  }
+  invisible(NULL)
 }
 
