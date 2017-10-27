afd5ab5c43f54dca19389169f61bb3f16cd97b8c
Stefan Milton Bache <stefan.bache@redbubble.com>
2017-10-27 14:12:45 +1100

    wip

--- R/record.R
+++ R/record.R
@@ -25,11 +25,12 @@ record <- function(message)
     answer <- readLines(n = 1, warn = FALSE)
     if (!any(c("y", "ye", "yes") %in% tolower(answer)))
       return()
-    
-    # git2r::stash()
-    # sha <-  "TODO"
-    # retrieve(sha)
-    stop("Not implemented yet for the case where we're not at master.")
+   
+    # TODO Can we do this without system? 
+    call_system("git", "stash")
+    sha <-  "TODO"
+    retrieve(sha)
+    call_system("git", "stash pop")
   }
 
   # Stage unstaged changes
