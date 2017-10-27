9a24d010fc34952fa3d1a5a9165f53a9cd64da88
Nick Golding <goldingn35@gmail.com>
2017-10-28 08:32:52 +1100

    error-handle and document starlog

--- R/starlogs.R
+++ R/starlogs.R
@@ -1,12 +1,29 @@
 #' Show repo history in starlogs
 #'
-#' TODO: Describe this better.
+#' Display your past record messages at starlogs.net. This requires internet
+#' access and only works for version control projects that have an online copy
 #'
-#' @return nothing
 #' @export
+#' @importFrom git2r remotes remote_url
+#' @importFrom utils browseURL
 starlog <- function() {
+  
   repo <- get_repo()
-  remote_url <- paste(strsplit(git2r::remote_url(repo, git2r::remotes(repo)),"/")[[1]][4:5], sep="", collapse="/")
-  browseURL(url = paste("http://starlogs.net/#",remote_url, sep='', collapse=""))
+  remote_urls <- git2r::remote_url(repo)
+  
+  if (some(remote_urls)) {
+    remote_url <- remote_urls[1] 
+    remote_parts <- strsplit(remote_url, "/")[[1]]
+    last_parts <- remote_parts[length(remote_parts) - 1:0]
+    starlogs_url <- sprintf("http://starlogs.net/#%s/%s",
+                   last_parts[1],
+                   last_parts[2])
+    browseURL(starlogs_url)
+    
+  } else {
+    message("this version control project has no online copies, ",
+            "so you can't use starlog just yet")
+  }
+  
 }
 
