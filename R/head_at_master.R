dc36d4c2af82ba9403b26b08a69b8db00eeae5dd
Stefan Milton Bache <stefan.bache@redbubble.com>
2017-10-27 11:40:39 +1100

    Remove whitespace

--- R/head_at_master.R
+++ R/head_at_master.R
@@ -1,5 +1,5 @@
 #' Check if HEAD is at master
-#' 
+#'
 #' @return logical indicating if head is at master.
 #'
 #' @importFrom githug git_revision
@@ -7,5 +7,5 @@
 #' @noRd
 head_at_master <- function()
 {
-  git_revision("HEAD")[1] == git_revision("master")[1] 
+  git_revision("HEAD")[1] == git_revision("master")[1]
 }
--- R/retrieve.R
+++ R/retrieve.R
@@ -7,32 +7,32 @@
 #' @param files vector or list files to retrive TODO: add this
 #'
 #' @importFrom git2r checkout commits
-#' 
+#'
 #' @return TODO
 #' @export
 retrieve <- function(sha, files = NULL) {
   repo <- get_repo()
-  
+
   # TODO scrub the existing changes.
   if (!is_clean(repo)) {
     stop("You need to record or scrub before you ",
          "can retrieve when there are changes.")
   }
-  
+
   timestamp <- gsub("[^0-9]", "", Sys.time())
   commits   <- git2r::commits(repo)
   shas      <- vapply(commits, `@`, character(1), "sha")
   index     <- which(sha == shas)
-  
+
   if (length(index) == 0) {
     stop("Can not find corresponding record.")
   }
-  
-  
+
+
   git2r::checkout(commits[[index[1]]], branch = timestamp, create = TRUE)
   git2r::checkout(branch = "master")
-  
-  msg <- sprintf("Retrieving previous state from sha: %s", sha) 
+
+  msg <- sprintf("Retrieving previous state from sha: %s", sha)
   branch <- git2r::branches()[[timestamp]]
   git2r::merge(branch, `--strategy-option` = "theirs", `-m` = msg)
 }
