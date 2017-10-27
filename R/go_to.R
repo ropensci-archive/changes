92c289acb5269a85c80a6f591dbca55ece315f64
Stefan Milton Bache <stefan.bache@redbubble.com>
2017-10-27 15:13:45 +1100

    Add go to

--- R/go_to.R
+++ R/go_to.R
@@ -2,11 +2,20 @@
 #'
 #' TODO Describe this better.
 #'
-#' @param where The snapshot to go to.
+#' @param sha The snapshot to go to.
 #'
 #' @return TODO
 #' @export
-go_to <- function(where)
+go_to <- function(sha)
 {
-
+  repo <- get_repo()
+  
+  commits   <- git2r::commits(repo)
+  shas      <- vapply(commits, function(x) x@sha, character(1))
+  
+  if (!sha %in shas) {
+    stop("The provided sha is not found.")
+  }
+  
+  invisible(git2r::checkout(repo, sha))
 }
