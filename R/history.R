2cd84a5bb1ae7d7f02f813f69aeab467ae52430f
Saras Windecker <windeckers@student.unimelb.edu.au>
2017-10-27 11:40:12 +1100

    add history function and print and plot defaults #17

--- R/history.R
+++ R/history.R
@@ -1,42 +1,40 @@
 #' Shows the commit log
 #'
-#' This function shows me
+#' This function shows the history of records made. 
 #'
-#' @param process_object process object obtained from process function
-#' @return decon list containing amended dataframe, bounds, model output, mass fractions
-#' @keywords thermogravimetry fraser-suzuki deconvolution
-#' @import git2r 
-#' @importFrom stats integrate setNames loess
+#' @return history object
+#' @keywords  
+#' @import git2r purrr
+#' @importFrom methods slot as
+#' This script borrows from the 'githug' package: https://github.com/jennybc/githug
 #' @examples
-#'
+#' 
 #' @export
 
 history <- function ( ) {
-  library('git2r')
+
   #repo <- get_repo()
   repo <- git2r::repository(getwd())
   records <- git2r::commits(repo)
   
-  
-  
-  obj <- 'A; B; C'
-  grViz("
-
-  digraph boxes_and_circles {
-
-  node [shape = circle]
-  obj
-
-  node [shape = box]
-  1
+  if (length(records) == 0) {
+    message("No records yet added.")
   }
-      
-        
-  ")
-    
+  
+  record_id <- as.list(seq(length(records), 1, length.out = length(records)))
+  raw_author <- purrr::map(records, methods::slot, "author")
+  author <- purrr::map(raw_author, methods::slot, 'name')
+  when <- purrr::map(raw_author, methods::slot, "when")
+  when <- purrr::map(when, ~ methods::as(.x, "POSIXct"))
+  when <- do.call(c, when)
+  email <- purrr::map_chr(raw_author, methods::slot, "email")
+  message <- purrr::map_chr(records, methods::slot, "message")
+  sha <- purrr::map_chr(records, methods::slot, "sha")
+  
+  log <- tibble::tibble(record_id, author, when, email, message, sha)
+  
+  structure(log, class = c('history', class(log)))
+  log
   
 }
-
-
-
 
