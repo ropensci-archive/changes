cccf4ff70031653553d72202e80a5701ce624c09
Nick Golding <goldingn35@gmail.com>
2017-10-29 14:02:52 +1100

    reattach head when moving back to tip

--- R/go_to.R
+++ R/go_to.R
@@ -16,4 +16,10 @@ go_to <- function (number) {
   commit <- git2r::revparse_single(repo, sha)
   git2r::checkout(commit)  # call_system("git", c("checkout", sha))
   
+  # if HEAD is now at master, reattach HEAD to master
+  if (head_at_master(repo)) {
+    master <- git2r::branches(repo)$master
+    git2r::checkout(master)
+  }
+  
 }
